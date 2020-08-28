08491bc791c9df89b7a76f05946d8a3a478f1934
Collosal Havoc <66965331+Collosalhavoc@users.noreply.github.com>
2020-08-28 09:23:49 +0530

    Update __main__.py

--- skylee/__main__.py
+++ skylee/__main__.py
@@ -37,7 +37,7 @@ Hey there! my name is *{dispatcher.bot.first_name}*. If you have any questions o
 I'm here to make your group management fun and easy!
 i have lots of handy features, such as flood control, a warning system, a note keeping system, and even replies on predetermined filters.
 
-Any issues or need help related to me? join our group [Ryumi support chat](https://t.me/Message_dump_group).
+Any issues or need help related to me? join our group [Ryumi support chat](https://t.me/messagedumpgroup).
 
 Wanna Add me to your Group? Just click the button below!
 """
@@ -47,7 +47,7 @@ buttons = [
         InlineKeyboardButton(
             text="Add to Group 👥", url="t.me/Ryumi_bot?startgroup=true"
         ),
-        InlineKeyboardButton(text="Updates 📢", url="https://t.me/Message_dump_group"),
+        InlineKeyboardButton(text="Updates 📢", url="https://t.me/messagedumpgroup"),
     ]
 ]
 
