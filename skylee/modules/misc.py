30ad220f94610443d9674f799179fedca017017f
Collosal Havoc <66965331+Collosalhavoc@users.noreply.github.com>
2020-08-30 22:18:54 +0530

    Update misc.py

--- skylee/modules/misc.py
+++ skylee/modules/misc.py
@@ -499,7 +499,7 @@ def stats(update, context):
     )
 
 @run_async 
-def ping(update,context):
+def ping(update, context):
     start_time = time.time()
     requests.get('https://api.telegram.org')
     end_time = time.time()
