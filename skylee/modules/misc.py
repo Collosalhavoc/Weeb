98b7e0e85e5a8ebbc68dcf1f07a194f4c5f9894f
Collosal Havoc <66965331+Collosalhavoc@users.noreply.github.com>
2020-08-31 01:28:39 +0530

    Update misc.py

--- skylee/modules/misc.py
+++ skylee/modules/misc.py
@@ -502,7 +502,7 @@ def stats(update, context):
 @typing_action
 def ping(update, context):
     start_time = time.time()
-    requests.get('https://api.telegram.org')
+    results=get('https://api.telegram.org').json()
     end_time = time.time()
     ping_time = round((end_time - start_time)*1000, 3)
     update.effective_message.reply_text("*Your current ping😶*\n`{}ms`".format(ping_time), parse_mode=ParseMode.MARKDOWN)
