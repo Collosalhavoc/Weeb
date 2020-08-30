f637f8ab132a66ee92cb619311dd21dcc94c1a82
Collosal Havoc <66965331+Collosalhavoc@users.noreply.github.com>
2020-08-31 01:05:15 +0530

    Update ping.py

--- skylee/modules/ping.py
+++ skylee/modules/ping.py
@@ -1,1 +1,13 @@
+import time
 
+from skylee import dispatcher
+
+@run_async 
+def ping(update, context):
+    start_time = time.time()
+    requests.get('https://api.telegram.org')
+    end_time = time.time()
+    ping_time = round((end_time - start_time)*1000, 3)
+    update.effective_message.reply_text("*Your current ping😶*\n`{}ms`".format(ping_time), parse_mode=ParseMode.MARKDOWN)
+    
+    
