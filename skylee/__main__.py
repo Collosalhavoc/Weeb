8cf393e43023aba47e4dbfb3b5139595c1c4a88e
Collosal Havoc <66965331+Collosalhavoc@users.noreply.github.com>
2020-08-30 13:48:19 +0530

    Update __main__.py

--- skylee/__main__.py
+++ skylee/__main__.py
@@ -34,7 +34,7 @@ from skylee.modules.helper_funcs.alternate import typing_action
 PM_START_TEXT = f"""
 Hey there! my name is *{dispatcher.bot.first_name}*. If you have any questions on how to use me, Click Help button.
 
-I'm here to make your group management fun and easy!
+I'm an anime themed bot and I am here to make your group management fun and easy!
 i have lots of handy features, such as flood control, a warning system, a note keeping system, and even replies on predetermined filters.
 
 Any issues or need help related to me? join our group [Ryumi support chat](https://t.me/messagedumpgroup).
