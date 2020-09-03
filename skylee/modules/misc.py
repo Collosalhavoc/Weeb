5b8a68e74452ad3008d8a071bad249ca35c5277d
Collosal Havoc <66965331+Collosalhavoc@users.noreply.github.com>
2020-09-03 18:44:27 +0530

    Update misc.py

--- skylee/modules/misc.py
+++ skylee/modules/misc.py
@@ -513,6 +513,68 @@ def ping(update, context):
         "*Pong!!!*\n`{}ms`".format(ping_time), parse_mode=ParseMode.MARKDOWN
     )
     
+    
+
+ 
+@run_async
+@typing_action
+@app.on_message(filters.user(AdminSettings) & filters.command("ocr", Command))
+
+OCR_SPACE_API_KEY = '30dd97e2b588957'
+async def ocr_space_file(filename,overlay=False,api_key=OCR_SPACE_API_KEY,language='eng'):
+    payload = {
+        'isOverlayRequired': overlay,
+        'apikey': api_key,
+        'language': language,
+    }
+    with open(filename, 'rb') as f:
+        r = requests.post(
+            'https://api.ocr.space/parse/image',
+            files={filename: f},
+            data=payload,
+        )
+    return r.json()
+def ocr(update, context):
+    cmd = message.context
+    lang_code = ''
+    if len(cmd) > 1:
+        lang_code = " ".join(cmd[1:])
+    elif len(cmd) == 1:
+        lang_code = 'eng'
+    replied = message.reply_to_message
+    if not replied:
+        await message.delete()
+        return
+    if replied.video:
+        await message.delete()
+        return
+    if replied.document:
+        await message.delete()
+        return
+    if replied.voice:
+        await message.delete()
+        return
+    if replied.audio:
+        await message.delete()
+        return
+    if replied.photo:
+        reply_p = replied.photo
+    elif replied.sticker:
+        reply_p = replied.sticker
+    downloaded_file_name = await client.download_media(reply_p, 'nana/cache/file.png')
+    test_file = await ocr_space_file(filename=downloaded_file_name,language=lang_code)
+    try:
+        ParsedText = test_file["ParsedResults"][0]["ParsedText"]
+    except BaseException as e:
+        await message.reply(e)
+    else:
+        if ParsedText == 'ParsedResults':
+            await message.delete()
+            return
+        else:
+            await message.reply(f"`{ParsedText}`")
+    os.remove(downloaded_file_name)
+    
 # /ip is for private use
 __help__ = """
 An "odds and ends" module for small, simple commands which don't really fit anywhere
