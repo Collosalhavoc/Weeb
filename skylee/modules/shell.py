24b3b90e10ade08e8864a8ec9e4a4f8c294c23ea
Collosal Havoc <66965331+Collosalhavoc@users.noreply.github.com>
2020-09-07 23:02:19 +0530

    update shell.py

--- skylee/modules/shell.py
+++ skylee/modules/shell.py
@@ -4,8 +4,8 @@ from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.ext.dispatcher import run_async
  
-from misaki import LOGGER, dispatcher
-from misaki.modules.helper_funcs.chat_status import dev_plus
+from skylee import LOGGER, dispatcher
+from skylee.modules.helper_funcs.chat_status import dev_plus
  
 @dev_plus
 @run_async
