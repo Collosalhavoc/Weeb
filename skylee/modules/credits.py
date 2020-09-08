096d27569a6423a01a25668284fb98884d3fc68b
Collosal Havoc <66965331+Collosalhavoc@users.noreply.github.com>
2020-09-09 00:54:02 +0530

    update credits.py

--- skylee/modules/credits.py
+++ skylee/modules/credits.py
@@ -7,36 +7,13 @@ __help__ = """
 
 *These persons helped me a lot in direct and indirect ways to build my bot*
 
+@dank_as_fuck
+@GeekyRash
+@pokurt
+@Dragsama
+@RealPhoenix
+@fermiparadox
 
-
-    animation_interval = 1
-
-    animation_ttl = range(0, 30)
-
-   # input_str = event.pattern_match.group(1)
-
-   # if input_str == "chu":
-
-    await event.edit("Credits......")
-
-    animation_chars = [
-        
-            "`Searching throughout Telegram`",
-            "`Users Found.`",
-            "`user1... dank guy\n@dank_as_fuck`",
-            "`user2... Phoenix guy\n@RealPhoenix`",
-            "`user3... base repo\n skylee`",    
-            "`user4....simp but good coder \n @pokurt`",
-           
-            "`These were some people who helped me knowimgly and unknowingly `"
-        ]
-
-
-    for i in animation_ttl:
-
-        await asyncio.sleep(animation_interval)
-
-        await event.edit(animation_chars[i % 30])
 """
 
 __mod_name__ = "Credits"
